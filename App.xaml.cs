5f3ffe16dfc38e4c42d48d2411f905cf1a2bd3f8
S3lv3r <fernanfc3098@gmail.com>
2025-05-24 01:32:56 -0600

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Jobbara
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
