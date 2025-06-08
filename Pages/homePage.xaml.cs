25e57dc31e88150b2ce18e7ab06c4fad1ee7e9ee
S3lv3r <fernanfc3098@gmail.com>
2025-06-08 17:07:31 -0600

    WIP: avances de mi funcionalidad

--- Pages/homePage.xaml.cs
+++ Pages/homePage.xaml.cs
@@ -4,9 +4,8 @@ public partial class homePage : ContentPage
 {
 	public homePage()
 	{
-		InitializeComponent();
+		InitializeComponent(); //All okey
 	}
-<<<<<<< HEAD
 	private async void OnGoToProfile(object sender, EventArgs e)
 	{
 		await Shell.Current.GoToAsync("//userProfile");
@@ -25,6 +24,4 @@ public partial class homePage : ContentPage
     {
         await Shell.Current.GoToAsync("//notificaciones");
     }
-=======
->>>>>>> 142eef0d389d5f4b6554b01b49634745e9e07cc3
 }
