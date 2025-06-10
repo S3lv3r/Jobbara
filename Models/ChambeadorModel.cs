ca0267c06c3f2599a11e725468ea5ddc3f5701e9
Daniel <dan07gar67@gmail.com>
2025-06-09 23:38:55 -0600

    El perfil sirve

---
+++ Models/ChambeadorModel.cs
@@ -0,0 +1,10 @@
+﻿public class ChambeadorModel
+{
+    public string username { get; set; }
+    public string oficio { get; set; }
+    public string ine { get; set; }
+    public string curp { get; set; }
+    public string domicilio { get; set; }
+    public string rfc { get; set; }
+    public bool isWorker { get; set; }
+}
